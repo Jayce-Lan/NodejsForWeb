e15c594091602481b9d5a961069115a8c17623e6
lanjiesi <jayce404@foxmail.com>
2021-11-27 01:14:34 +0800

    测试提交

---
+++ JavaScript/test.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2021-11-27 01:12:44
+ * @LastEditTime: 2021-11-27 01:14:01
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /NodejsForWeb/JavaScript/test.js
+ */
+console.log("测试提交");
